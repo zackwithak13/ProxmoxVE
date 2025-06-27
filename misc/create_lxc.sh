4c1af53e158a61d3b3c2e07717aea692800cf8c4
Zack Ruppert <dev@zack.rupinga.com>
2025-06-27 13:57:38 -0400

    prayin

--- misc/create_lxc.sh
+++ misc/create_lxc.sh
@@ -9,11 +9,11 @@
 # if [ "$VERBOSE" == "yes" ]; then set -x; fi
 
 if command -v curl >/dev/null 2>&1; then
-  source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/core.func)
+  source <(curl -fsSL https://raw.githubusercontent.com/zackwithak13/ProxmoxVE/refs/heads/feat/uhf-server-lxc/misc/core.func)
   load_functions
   #echo "(create-lxc.sh) Loaded core.func via curl"
 elif command -v wget >/dev/null 2>&1; then
-  source <(wget -qO- https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/core.func)
+  source <(wget -qO- https://raw.githubusercontent.com/zackwithak13/ProxmoxVE/refs/heads/feat/uhf-server-lxc/misc/core.func)
   load_functions
   #echo "(create-lxc.sh) Loaded core.func via wget"
 fi
