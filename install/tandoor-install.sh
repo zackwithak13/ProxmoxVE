3b76211c122bf3dbc8fb6d2dc9c52fb8b5ae616b
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-07 19:38:49 +0200

    Tandoor v2 (#6635)

--- install/tandoor-install.sh
+++ install/tandoor-install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2025 tteck
-# Author: tteck
-# Co-Author: MickLesk (Canbiz)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: MickLesk (Canbiz)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://tandoor.dev/
 
@@ -17,6 +16,7 @@ update_os
 msg_info "Installing Dependencies (Patience)"
 $STD apt-get install -y --no-install-recommends \
   build-essential \
+  python3 \
   libpq-dev \
   libmagic-dev \
   libzbar0 \
@@ -24,72 +24,72 @@ $STD apt-get install -y --no-install-recommends \
   libsasl2-dev \
   libldap2-dev \
   libssl-dev \
-  git \
-  make \
   pkg-config \
   libxmlsec1-dev \
   libxml2-dev \
   libxmlsec1-openssl
 msg_ok "Installed Dependencies"
 
-msg_info "Setup Python3"
-$STD apt-get install -y \
-  python3 \
-  python3-dev \
-  python3-setuptools \
-  python3-pip \
-  python3-xmlsec
-rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
-msg_ok "Setup Python3"
+NODE_VERSION="20" NODE_MODULE="yarn" setup_nodejs
+fetch_and_deploy_gh_release "tandoor" "TandoorRecipes/recipes" "tarball" "latest" "/opt/tandoor"
+PG_VERSION="16" setup_postgresql
+PYTHON_VERSION="3.13" setup_uv
 
-NODE_VERSION="20" NODE_MODULE="yarn@latest" setup_nodejs
-
-msg_info "Installing Tandoor (Patience)"
-$STD git clone https://github.com/TandoorRecipes/recipes -b master /opt/tandoor
-mkdir -p /opt/tandoor/{config,api,mediafiles,staticfiles}
-$STD pip3 install -r /opt/tandoor/requirements.txt
-cd /opt/tandoor/vue
-$STD yarn install
-$STD yarn build
-curl -fsSL "https://raw.githubusercontent.com/TandoorRecipes/recipes/develop/.env.template" -o "/opt/tandoor/.env"
+msg_info "Set up PostgreSQL Database"
 DB_NAME=db_recipes
 DB_USER=tandoor
-DB_ENCODING=utf8
-DB_TIMEZONE=UTC
-secret_key=$(openssl rand -base64 45 | sed 's/\//\\\//g')
+SECRET_KEY=$(openssl rand -base64 45 | sed 's/\//\\\//g')
 DB_PASS="$(openssl rand -base64 18 | cut -c1-13)"
-sed -i -e "s|SECRET_KEY=.*|SECRET_KEY=$secret_key|g" \
-  -e "s|POSTGRES_HOST=.*|POSTGRES_HOST=localhost|g" \
-  -e "s|POSTGRES_PASSWORD=.*|POSTGRES_PASSWORD=$DB_PASS|g" \
-  -e "s|POSTGRES_DB=.*|POSTGRES_DB=$DB_NAME|g" \
-  -e "s|POSTGRES_USER=.*|POSTGRES_USER=$DB_USER|g" \
-  -e "\$a\STATIC_URL=/staticfiles/" /opt/tandoor/.env
-cd /opt/tandoor
-$STD python3 version.py
-msg_ok "Installed Tandoor"
-
-msg_info "Install/Set up PostgreSQL Database"
-curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
-echo "deb https://apt.postgresql.org/pub/repos/apt bookworm-pgdg main" >/etc/apt/sources.list.d/pgdg.list
-$STD apt-get update
-$STD apt-get install -y postgresql-16
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0;"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
-echo "" >>~/tandoor.creds
-echo -e "Tandoor Database Name: \e[32m$DB_NAME\e[0m" >>~/tandoor.creds
-echo -e "Tandoor Database User: \e[32m$DB_USER\e[0m" >>~/tandoor.creds
-echo -e "Tandoor Database Password: \e[32m$DB_PASS\e[0m" >>~/tandoor.creds
-export $(cat /opt/tandoor/.env | grep "^[^#]" | xargs)
-/usr/bin/python3 /opt/tandoor/manage.py migrate >/dev/null 2>&1
-/usr/bin/python3 /opt/tandoor/manage.py collectstatic --no-input >/dev/null 2>&1
-/usr/bin/python3 /opt/tandoor/manage.py collectstatic_js_reverse >/dev/null 2>&1
+{
+  echo "Tandoor-Credentials"
+  echo "Tandoor Database Name: $DB_NAME"
+  echo "Tandoor Database User: $DB_USER"
+  echo "Tandoor Database Password: $DB_PASS"
+} >>~/tandoor.creds
 msg_ok "Set up PostgreSQL Database"
 
+msg_info "Setup Tandoor"
+mkdir -p /opt/tandoor/{config,api,mediafiles,staticfiles}
+cd /opt/tandoor
+$STD uv venv .venv --python=python3
+$STD uv pip install -r requirements.txt --python .venv/bin/python
+cd /opt/tandoor/vue3
+$STD yarn install
+$STD yarn build
+cat <<EOF >/opt/tandoor/.env
+SECRET_KEY=$SECRET_KEY
+TZ=Europe/Berlin
+
+DB_ENGINE=django.db.backends.postgresql
+POSTGRES_HOST=localhost
+POSTGRES_DB=$DB_NAME
+POSTGRES_PORT=5432
+POSTGRES_USER=$DB_USER
+POSTGRES_PASSWORD=$DB_PASS
+
+STATIC_URL=/staticfiles/
+MEDIA_URL=/mediafiles/
+EOF
+
+TANDOOR_VERSION="$(curl -s https://api.github.com/repos/TandoorRecipes/recipes/releases/latest | jq -r .tag_name)"
+cat <<EOF >/opt/tandoor/cookbook/version_info.py
+TANDOOR_VERSION = "$TANDOOR_VERSION"
+TANDOOR_REF = "bare-metal"
+VERSION_INFO = []
+EOF
+
+cd /opt/tandoor
+$STD /opt/tandoor/.venv/bin/python manage.py migrate
+$STD /opt/tandoor/.venv/bin/python manage.py collectstatic --no-input
+msg_ok "Installed Tandoor"
+
 msg_info "Creating Services"
-cat <<EOF >/etc/systemd/system/gunicorn_tandoor.service
+cat <<EOF >/etc/systemd/system/tandoor.service
 [Unit]
 Description=gunicorn daemon for tandoor
 After=network.target
@@ -100,7 +100,7 @@ Restart=always
 RestartSec=3
 WorkingDirectory=/opt/tandoor
 EnvironmentFile=/opt/tandoor/.env
-ExecStart=/usr/local/bin/gunicorn --error-logfile /tmp/gunicorn_err.log --log-level debug --capture-output --bind unix:/opt/tandoor/tandoor.sock recipes.wsgi:application
+ExecStart=/opt/tandoor/.venv/bin/gunicorn --error-logfile /tmp/gunicorn_err.log --log-level debug --capture-output --bind unix:/opt/tandoor/tandoor.sock recipes.wsgi:application
 
 [Install]
 WantedBy=multi-user.target
@@ -109,8 +109,8 @@ EOF
 cat <<'EOF' >/etc/nginx/conf.d/tandoor.conf
 server {
     listen 8002;
-    #access_log /var/log/nginx/access.log;
-    #error_log /var/log/nginx/error.log;
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
     client_max_body_size 128M;
     # serve media files
     location /static/ {
@@ -123,12 +123,15 @@ server {
 
     location / {
         proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
         proxy_pass http://unix:/opt/tandoor/tandoor.sock;
     }
 }
 EOF
 systemctl reload nginx
-systemctl enable -q --now gunicorn_tandoor
+systemctl enable -q --now tandoor
 msg_ok "Created Services"
 
 motd_ssh
