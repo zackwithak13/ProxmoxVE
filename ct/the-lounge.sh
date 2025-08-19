01fe1b1f627d18c8266af851e196f3b5772f07ce
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-08-19 15:01:41 +0200

    Refactor (#6958)

--- ct/the-lounge.sh
+++ ct/the-lounge.sh
@@ -27,33 +27,15 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-  if ! dpkg -l build-essential >/dev/null 2>&1; then
-    $STD apt-get update
-    $STD apt-get install -y build-essential
-  fi
-  if ! npm list -g node-gyp >/dev/null 2>&1; then
-    $STD npm install -g node-gyp
-  fi
+
   RELEASE=$(curl -fsSL https://api.github.com/repos/thelounge/thelounge-deb/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+  if [[ ! -f ~/.thelounge ]] || [[ "${RELEASE}" != "$(cat ~/.thelounge)" ]]; then
     msg_info "Stopping Service"
     systemctl stop thelounge
     msg_ok "Stopped Service"
 
-    msg_info "Updating ${APP} to v${RELEASE}"
-    $STD apt-get install --only-upgrade nodejs
-    cd /opt
-    curl -fsSL "https://github.com/thelounge/thelounge-deb/releases/download/v${RELEASE}/thelounge_${RELEASE}_all.deb" -o $(basename "https://github.com/thelounge/thelounge-deb/releases/download/v${RELEASE}/thelounge_${RELEASE}_all.deb")
-    dpkg -i ./thelounge_${RELEASE}_all.deb
-    msg_ok "Updated ${APP} to v${RELEASE}"
+    fetch_and_deploy_gh_release "thelounge" "thelounge/thelounge-deb" "binary"
 
-    msg_info "Starting Service"
-    systemctl start thelounge
-    msg_ok "Started Service"
-
-    msg_info "Cleaning up"
-    rm -rf "/opt/thelounge_${RELEASE}_all.deb"
-    msg_ok "Cleaned"
     msg_ok "Updated Successfully"
   else
     msg_ok "No update required.  ${APP} is already at v${RELEASE}."
--- install/the-lounge-install.sh
+++ install/the-lounge-install.sh
@@ -13,26 +13,12 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  build-essential
-msg_ok "Installed Dependencies"
-
-NODE_VERSION="22" NODE_MODULE="yarn@latest,node-gyp" setup_nodejs
-
-msg_info "Installing The Lounge"
-cd /opt
-RELEASE=$(curl -fsSL https://api.github.com/repos/thelounge/thelounge-deb/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-curl -fsSL "https://github.com/thelounge/thelounge-deb/releases/download/v${RELEASE}/thelounge_${RELEASE}_all.deb" -o "./thelounge_${RELEASE}_all.deb"
-$STD dpkg -i ./thelounge_${RELEASE}_all.deb
-echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
-msg_ok "Installed The Lounge"
+fetch_and_deploy_gh_release "thelounge" "thelounge/thelounge-deb" "binary"
 
 motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf /opt/thelounge_${RELEASE}_all.deb
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
