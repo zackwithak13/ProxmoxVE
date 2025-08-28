522f427677632d78fdd594236da6bf9c9a801f72
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-28 11:23:35 +0200

    Flaresolverr: Pin to 3.3.25 (Python Issue) (#7248)

--- ct/flaresolverr.sh
+++ ct/flaresolverr.sh
@@ -35,7 +35,7 @@ function update_script() {
     msg_ok "Stopped service"
 
     rm -rf /opt/flaresolverr
-    fetch_and_deploy_gh_release "flaresolverr" "FlareSolverr/FlareSolverr" "prebuild" "latest" "/opt/flaresolverr" "flaresolverr_linux_x64.tar.gz"
+    fetch_and_deploy_gh_release "flaresolverr" "FlareSolverr/FlareSolverr" "prebuild" "v3.3.25" "/opt/flaresolverr" "flaresolverr_linux_x64.tar.gz"
 
     msg_info "Starting service"
     systemctl start flaresolverr
