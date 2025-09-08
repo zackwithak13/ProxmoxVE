d450e263f0955c6645a4f96de95bcb7df89a046f
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-09-08 11:58:29 +0200

    Update (#7473)

--- install/rustdeskserver-install.sh
+++ install/rustdeskserver-install.sh
@@ -18,18 +18,6 @@ fetch_and_deploy_gh_release "rustdesk-hbbs" "rustdesk/rustdesk-server" "binary" 
 fetch_and_deploy_gh_release "rustdesk-utils" "rustdesk/rustdesk-server" "binary" "latest" "/opt/rustdesk" "rustdesk-server-utils*amd64.deb"
 fetch_and_deploy_gh_release "rustdesk-api" "lejianwen/rustdesk-api" "binary" "latest" "/opt/rustdesk" "rustdesk-api-server*amd64.deb"
 
-msg_info "Configuring RustDesk Server"
-ADMINPASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
-cd /var/lib/rustdesk-api
-$STD rustdesk-api reset-admin-pwd $ADMINPASS
-{
-  echo "RustDesk WebUI"
-  echo ""
-  echo "Username: admin"
-  echo "Password: $ADMINPASS"
-} >>~/rustdesk.creds
-msg_ok "Configured RustDesk Server"
-
 motd_ssh
 customize
 
