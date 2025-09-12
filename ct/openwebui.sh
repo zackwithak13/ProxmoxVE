f4df8e2ccc6a55aba3a28b54c96b1e2bd63bed84
webmogul1 <camoura10@gmail.com>
2025-09-12 06:30:00 -0400

    Update openwebui.sh (#7582)

--- ct/openwebui.sh
+++ ct/openwebui.sh
@@ -33,11 +33,17 @@ function update_script() {
     OLLAMA_VERSION=$(ollama -v | awk '{print $NF}')
     RELEASE=$(curl -s https://api.github.com/repos/ollama/ollama/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4)}')
     if [ "$OLLAMA_VERSION" != "$RELEASE" ]; then
+      msg_info "Stopping Service"
+      systemctl stop ollama
+      msg_ok "Stopped Service"
+      curl -fsSLO https://ollama.com/download/ollama-linux-amd64.tgz
       rm -rf /usr/lib/ollama
       rm -rf /usr/bin/ollama
-      curl -fsSLO https://ollama.com/download/ollama-linux-amd64.tgz
       tar -C /usr -xzf ollama-linux-amd64.tgz
       rm -rf ollama-linux-amd64.tgz
+      msg_info "Starting Service"
+      systemctl start ollama
+      msg_info "Started Service"
       msg_ok "Ollama updated to version $RELEASE"
     else
       msg_ok "Ollama is already up to date."
