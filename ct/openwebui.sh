e3aabd81a35bb62a1529f8e44d3ba37ecb3e3580
HeedfulCrayon <nathan.borup@gmail.com>
2025-09-21 03:40:33 -0600

    Update Ollama Installer in OpenWebUI to resume downloads if interrupted (#7779)
    * Fix curl command options for downloading file
    
    * Update curl command for Ollama installation

--- ct/openwebui.sh
+++ ct/openwebui.sh
@@ -36,7 +36,7 @@ function update_script() {
       msg_info "Stopping Service"
       systemctl stop ollama
       msg_ok "Stopped Service"
-      curl -fsSLO https://ollama.com/download/ollama-linux-amd64.tgz
+      curl -fsCSLO https://ollama.com/download/ollama-linux-amd64.tgz
       rm -rf /usr/lib/ollama
       rm -rf /usr/bin/ollama
       tar -C /usr -xzf ollama-linux-amd64.tgz
