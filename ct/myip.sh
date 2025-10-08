5716eb3086bf0e91ffaab71bf90643d631b317fa
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-10-08 15:37:37 +0200

    Increase resources (#8199)

--- ct/myip.sh
+++ ct/myip.sh
@@ -7,8 +7,8 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 APP="MyIP"
 var_tags="${var_tags:-network}"
-var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-512}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-1024}"
 var_disk="${var_disk:-4}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
@@ -20,29 +20,29 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -d /opt/myip ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-
-    if check_for_gh_release "myip" "jason5ng32/MyIP"; then
-        msg_info "Stopping Services"
-        systemctl stop myip
-        msg_ok "Stopped Services"
-
-        cp /opt/myip/.env /opt
-        CLEAN_INSTALL=1 fetch_and_deploy_gh_release "myip" "jason5ng32/MyIP" "tarball"
-        mv /opt/.env /opt/myip
-
-        msg_info "Starting Services"
-        systemctl start myip
-        msg_ok "Started Services"
-        msg_ok "Updated Successfully"
-    fi
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/myip ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+
+  if check_for_gh_release "myip" "jason5ng32/MyIP"; then
+    msg_info "Stopping Services"
+    systemctl stop myip
+    msg_ok "Stopped Services"
+
+    cp /opt/myip/.env /opt
+    CLEAN_INSTALL=1 fetch_and_deploy_gh_release "myip" "jason5ng32/MyIP" "tarball"
+    mv /opt/.env /opt/myip
+
+    msg_info "Starting Services"
+    systemctl start myip
+    msg_ok "Started Services"
+    msg_ok "Updated Successfully"
+  fi
+  exit
 }
 
 start
