659ea817405726257f3717d01e64d21390524c8f
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-10-08 06:44:57 +0200

    ZeroTier One: Fix install output (#8179)

--- install/zerotier-one-install.sh
+++ install/zerotier-one-install.sh
@@ -14,10 +14,14 @@ network_check
 update_os
 
 msg_info "Setting up Zerotier-One"
-curl -fsSL 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import &&
-  if z="$(curl -fsSL 'https://install.zerotier.com/' | gpg)"; then
-    echo "$z" | sudo bash
-  fi
+curl -fsSL https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg | gpg --import >/dev/null 2>&1
+curl -fsSL https://install.zerotier.com -o /tmp/zerotier-install.sh
+if gpg --verify /tmp/zerotier-install.sh >/dev/null 2>&1; then
+  $STD bash /tmp/zerotier-install.sh
+else
+  msg_warn "Could not verify signature of Zerotier-One install script. Exiting..."
+  exit 1
+fi
 msg_ok "Setup Zerotier-One"
 
 msg_info "Setting up UI"
