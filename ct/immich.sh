30f462b62a7cca862f90acd8dce5c869aabdb999
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-10-15 23:56:00 +0200

    fix immich version

--- ct/immich.sh
+++ ct/immich.sh
@@ -93,7 +93,7 @@ EOF
     msg_ok "Image-processing libraries up to date"
   fi
 
-  RELEASE="2.1.1"
+  RELEASE="2.1.0"
   if check_for_gh_release "immich" "immich-app/immich" "${RELEASE}"; then
     msg_info "Stopping Services"
     systemctl stop immich-web
