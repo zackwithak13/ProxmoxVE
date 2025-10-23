3b9623032773ccc6dec646b6ef23c0fc56b5e071
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-10-23 08:28:38 -0700

    Various Downgrades to Debian 12 (MySQL / OMW / Technitium)  (#8595)
    * downgrades deb12
    
    * fixes
    
    * Update inventree.sh
    
    * fix format
    
    * Update Debian version to 12 in omv and technitiumdns configs
    
    Changed the Debian version from 13 to 12 in both omv.json and technitiumdns.json to reflect the correct OS version for these configurations.

--- install/mysql-install.sh
+++ install/mysql-install.sh
@@ -63,27 +63,7 @@ msg_ok "MySQL Server configured"
 
 read -r -p "${TAB3}Would you like to add PhpMyAdmin? <y/N> " prompt
 if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-  msg_info "Installing phpMyAdmin"
-  $STD apt install -y \
-    apache2 \
-    php \
-    php-mysqli \
-    php-mbstring \
-    php-zip \
-    php-gd \
-    php-json \
-    php-curl
-
-  curl -fsSL "https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-all-languages.tar.gz" -o "phpMyAdmin-5.2.2-all-languages.tar.gz"
-  mkdir -p /var/www/html/phpMyAdmin
-  tar xf phpMyAdmin-5.2.2-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
-  cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
-  SECRET=$(openssl rand -base64 24)
-  sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
-  chmod 660 /var/www/html/phpMyAdmin/config.inc.php
-  chown -R www-data:www-data /var/www/html/phpMyAdmin
-  systemctl restart apache2
-  msg_ok "Installed phpMyAdmin"
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/addon/phpmyadmin.sh)"
 fi
 
 msg_info "Start Service"
