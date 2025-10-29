4bf2846efa35b6a3053cfc6b4dc9610c4a5f9b20
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-10-29 14:00:16 -0700

    typo

--- install/kimai-install.sh
+++ install/kimai-install.sh
@@ -41,7 +41,7 @@ $STD mariadb -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUS
 } >>~/kimai.creds
 msg_ok "Set up database"
 
-fetch_and_deploy_gh_release "Kimai" "kimai/kimai"
+fetch_and_deploy_gh_release "kimai" "kimai/kimai"
 
 msg_info "Installing Kimai"
 cd /opt/kimai
