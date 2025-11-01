d8618652d96009d3d73f23392c5b3cfa22045dc7
Chris <punk.sand7393@fastmail.com>
2025-11-01 08:44:21 -0400

    Immich: Pin version to 2.2.1 (#8800)

--- install/immich-install.sh
+++ install/immich-install.sh
@@ -288,7 +288,7 @@ GEO_DIR="${INSTALL_DIR}/geodata"
 mkdir -p "$INSTALL_DIR"
 mkdir -p {"${APP_DIR}","${UPLOAD_DIR}","${GEO_DIR}","${INSTALL_DIR}"/cache}
 
-fetch_and_deploy_gh_release "immich" "immich-app/immich" "tarball" "v2.2.0" "$SRC_DIR"
+fetch_and_deploy_gh_release "immich" "immich-app/immich" "tarball" "v2.2.1" "$SRC_DIR"
 
 msg_info "Installing ${APPLICATION} (patience)"
 
