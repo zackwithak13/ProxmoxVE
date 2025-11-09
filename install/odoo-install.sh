0ab91cc4c14e023642c05b36cdc227afecf14e99
akileos <138346+akileos@users.noreply.github.com>
2025-11-09 13:32:33 -0400

    Add wkhtmltopdf to Odoo installation dependencies (#9010)
    Required for invoice/ticket generation

--- install/odoo-install.sh
+++ install/odoo-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt install -y python3-lxml
+$STD apt install -y python3-lxml wkhtmltopdf
 curl -fsSL "http://archive.ubuntu.com/ubuntu/pool/universe/l/lxml-html-clean/python3-lxml-html-clean_0.1.1-1_all.deb" -o /opt/python3-lxml-html-clean.deb
 $STD dpkg -i /opt/python3-lxml-html-clean.deb
 msg_ok "Installed Dependencies"
