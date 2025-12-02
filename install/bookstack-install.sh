183dc7ac699e945eef8a486c8fd1ca969c153850
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-12-02 11:03:56 +0100

    Refactor: BookStack (#9567)
    * Refactor
    
    * Fix db names

--- install/bookstack-install.sh
+++ install/bookstack-install.sh
@@ -13,42 +13,24 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies (Patience)"
-$STD apt-get install -y \
-  apache2 \
-  make
+msg_info "Installing Dependencies"
+$STD apt install -y make
 msg_ok "Installed Dependencies"
 
 PHP_MODULE="ldap,tidy,bz2,mysqli" PHP_FPM="YES" PHP_APACHE="YES" PHP_VERSION="8.3" setup_php
-
 setup_composer
 setup_mariadb
-
-msg_info "Setting up Database"
-DB_NAME=bookstack
-DB_USER=bookstack
-DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
-$STD mariadb -u root -e "CREATE DATABASE $DB_NAME;"
-$STD mariadb -u root -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
-$STD mariadb -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUSH PRIVILEGES;"
-{
-  echo "Bookstack-Credentials"
-  echo "Bookstack Database User: $DB_USER"
-  echo "Bookstack Database Password: $DB_PASS"
-  echo "Bookstack Database Name: $DB_NAME"
-} >>~/bookstack.creds
-msg_ok "Set up database"
-
+MARIADB_DB_NAME="bookstack_db" MARIADB_DB_USER="bookstack_user" setup_mariadb_db
 fetch_and_deploy_gh_release "bookstack" "BookStackApp/BookStack"
-LOCAL_IP="$(hostname -I | awk '{print $1}')"
+import_local_ip
 
 msg_info "Configuring Bookstack (Patience)"
 cd /opt/bookstack
 cp .env.example .env
 sudo sed -i "s|APP_URL=.*|APP_URL=http://$LOCAL_IP|g" /opt/bookstack/.env
-sudo sed -i "s/DB_DATABASE=.*/DB_DATABASE=$DB_NAME/" /opt/bookstack/.env
-sudo sed -i "s/DB_USERNAME=.*/DB_USERNAME=$DB_USER/" /opt/bookstack/.env
-sudo sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$DB_PASS/" /opt/bookstack/.env
+sudo sed -i "s/DB_DATABASE=.*/DB_DATABASE=$MARIADB_DB_NAME/" /opt/bookstack/.env
+sudo sed -i "s/DB_USERNAME=.*/DB_USERNAME=$MARIADB_DB_USER/" /opt/bookstack/.env
+sudo sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$MARIADB_DB_PASS/" /opt/bookstack/.env
 $STD composer install --no-dev --no-plugins --no-interaction
 $STD php artisan key:generate --no-interaction --force
 $STD php artisan migrate --no-interaction --force
