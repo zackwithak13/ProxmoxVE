7255edc21b6eb5dce0185cb54d5ca3eaa4e492aa
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-12-11 12:31:37 +0100

    Fixes (#9872)

--- ct/syncthing.sh
+++ ct/syncthing.sh
@@ -27,9 +27,10 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-  msg_info "Updating ${APP} LXC"
+
+  msg_info "Updating Syncthing"
   $STD apt update
-  $STD apt -y upgrade
+  $STD apt upgrade -y
   msg_ok "Updated successfully!"
   exit
 }
--- install/syncthing-install.sh
+++ install/syncthing-install.sh
@@ -13,26 +13,20 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Setting up Syncthing repo"
-mkdir -p /usr/share/keyrings
-curl -fsSL "https://syncthing.net/release-key.gpg" -o /usr/share/keyrings/syncthing-archive-keyring.gpg
-cat <<EOF >/etc/apt/sources.list.d/syncthing.sources
-Types: deb
-URIs: https://apt.syncthing.net/
-Suites: syncthing
-Components: stable-v2
-Signed-By: /usr/share/keyrings/syncthing-archive-keyring.gpg
-EOF
-$STD apt update
-msg_ok "Set up Syncthing repo"
+setup_deb822_repo \
+  "syncthing" \
+  "https://syncthing.net/release-key.gpg" \
+  "https://apt.syncthing.net/" \
+  "syncthing" \
+  "stable-v2"
 
-msg_info "Installing Syncthing"
+msg_info "Setting up Syncthing"
 $STD apt install -y syncthing
 systemctl enable -q --now syncthing@root
 sleep 5
 sed -i "{s/127.0.0.1:8384/0.0.0.0:8384/g}" /root/.local/state/syncthing/config.xml
 systemctl restart syncthing@root
-msg_ok "Installed Syncthing"
+msg_ok "Setup Syncthing"
 
 motd_ssh
 customize
