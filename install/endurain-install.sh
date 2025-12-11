38a9ad8e097afacff867a50e70cf670d96f1fa71
Johann Grobe <123082619+johanngrobe@users.noreply.github.com>
2025-12-11 07:11:45 +0100

    endurain: remove unneeded deps (#9855)
    * Update endurain-install.sh
    
    updated deps for v0.16.0. MariaDB support was dropped.
    
    * chore: cosmetics
    
    ---------
    
    Co-authored-by: Tobias <96661824+CrazyWolf13@users.noreply.github.com>

--- install/endurain-install.sh
+++ install/endurain-install.sh
@@ -14,10 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt install -y \
-  default-libmysqlclient-dev \
-  build-essential \
-  pkg-config
+$STD apt install -y build-essential
 msg_ok "Installed Dependencies"
 
 PYTHON_VERSION="3.13" setup_uv
