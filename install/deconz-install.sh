468b5fbc38a6c79ff5dd3688036464b04a7e6737
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2026-01-08 14:00:48 +0100

    deCONZ: Bump to Trixie base (#10643)
    * Bump to Trixie
    
    * Update
    
    * Update

--- install/deconz-install.sh
+++ install/deconz-install.sh
@@ -14,17 +14,18 @@ network_check
 update_os
 
 msg_info "Setting Phoscon Repository"
-VERSION="$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)"
-curl -fsSL "http://phoscon.de/apt/deconz.pub.key" >/etc/apt/trusted.gpg.d/deconz.pub.asc
-echo "deb [arch=amd64] http://phoscon.de/apt/deconz $VERSION main" >/etc/apt/sources.list.d/deconz.list
+setup_deb822_repo \
+  "deconz" \
+  "http://phoscon.de/apt/deconz.pub.key" \
+  "http://phoscon.de/apt/deconz" \
+  "generic"
 msg_ok "Setup Phoscon Repository"
 
 msg_info "Installing deConz"
 libssl=$(curl -fsSL "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/" | grep -o 'libssl1\.1_1\.1\.1f-1ubuntu2\.2[^"]*amd64\.deb' | head -n1)
 curl -fsSL "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/$libssl" -o "$libssl"
 $STD dpkg -i "$libssl"
-$STD apt-get update
-$STD apt-get install -y deconz
+$STD apt install -y deconz
 rm -rf "$libssl"
 msg_ok "Installed deConz"
 
