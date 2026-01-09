02bbfa58519abcdac8ac6b330ea1f06f18ca27a5
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2026-01-09 15:19:05 +0100

    Fix (#10664)

--- ct/deconz.sh
+++ ct/deconz.sh
@@ -23,7 +23,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -f /etc/apt/sources.list.d/deconz.list ]]; then
+  if [[ ! -f /etc/apt/sources.list.d/deconz.list && ! -f /etc/apt/sources.list.d/deconz.sources ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
